686a685d4890ac4173fdb0f33767bdd97f6755ad
dxxyo <komangtagnan@gmail.com>
2023-08-10 00:46:53 +0700

    yeah

---
+++ script.js
@@ -0,0 +1,330 @@
+
+const datas = [
+    {
+        "id": "800935c1-39ac-4093-9e83-53b8932b0691",
+        "name": "Unbranded Soft Towels",
+        "status": "EDITING",
+        "type": "training",
+        "createdOn": "2022-06-15T02:11:04.149Z",
+        "archived": false
+    },
+    {
+        "id": "28660a7e-a41f-4167-8c4b-1da56bafd9a3",
+        "name": "Intelligent Rubber Ball",
+        "status": "INCOMPLETE",
+        "type": "educational",
+        "createdOn": "2022-06-15T02:11:04.149Z",
+        "archived": false
+    },
+    {
+        "id": "daf10306-6d80-446e-923c-d5b35554b17d",
+        "name": "Intelligent Fresh Bike",
+        "status": "EDITING",
+        "type": "educational",
+        "createdOn": "2022-05-15T02:11:04.149Z",
+        "archived": true
+    },
+    {
+        "id": "12060e5b-7be4-41c5-ad46-85c310bfb8cb",
+        "name": "Tasty Fresh Fish",
+        "status": "FEEDBACK",
+        "type": "training",
+        "createdOn": "2022-06-15T02:11:04.197Z",
+        "archived": true
+    },
+    {
+        "id": "844b14ce-330b-4c00-9291-b76a236bc088",
+        "name": "Intelligent Frozen Sausages",
+        "status": "FEEDBACK",
+        "type": "educational",
+        "createdOn": "2022-06-15T02:11:04.197Z",
+        "archived": true
+    },
+    {
+        "id": "dff82950-272f-4fc1-bf4a-ea9409a275dc",
+        "name": "Small Cotton Chips",
+        "status": "FEEDBACK",
+        "type": "testimonial",
+        "createdOn": "2022-06-15T02:11:04.198Z",
+        "archived": false
+    },
+    {
+        "id": "eb99863d-860e-4492-9694-143b07472009",
+        "name": "Sleek Wooden Gloves",
+        "status": "EDITING",
+        "type": "training",
+        "createdOn": "2022-06-15T02:11:04.198Z",
+        "archived": false
+    },
+    {
+        "id": "79955432-006a-4d12-b89b-c9c4cceb2f54",
+        "name": "Licensed Steel Bike",
+        "status": "SHOOTING",
+        "type": "testimonial",
+        "createdOn": "2022-06-15T02:11:04.198Z",
+        "archived": false
+    },
+    {
+        "id": "c8168731-8ce4-48f8-8cb3-c82be62e9c9d",
+        "name": "Fantastic Steel Shirt",
+        "status": "FEEDBACK",
+        "type": "educational",
+        "createdOn": "2022-06-15T02:11:04.206Z",
+        "archived": false
+    },
+    {
+        "id": "2b49a2be-2567-4828-98af-d1c2fd7c6670",
+        "name": "Electronic Plastic Chair",
+        "status": "FEEDBACK",
+        "type": "training",
+        "createdOn": "2022-06-15T02:11:04.206Z",
+        "archived": true
+    },
+    {
+        "id": "3fcf92e3-0bd5-4d79-a915-aa5096e1bd5c",
+        "name": "Electronic Granite Pants",
+        "status": "INCOMPLETE",
+        "type": "educational",
+        "createdOn": "2022-06-15T02:11:04.207Z",
+        "archived": false
+    },
+    {
+        "id": "170ba32c-3bc9-40e7-b4d4-e28643750e36",
+        "name": "Handmade Plastic Fish",
+        "status": "COMPLETED",
+        "type": "educational",
+        "createdOn": "2022-06-15T02:11:04.207Z",
+        "archived": true
+    },
+    {
+        "id": "2a04211e-c689-4030-bd06-ef88f67c4137",
+        "name": "Recycled Plastic Shoes",
+        "status": "INCOMPLETE",
+        "type": "recreational",
+        "createdOn": "2022-06-15T02:11:04.207Z",
+        "archived": false
+    },
+    {
+        "id": "989de212-857e-4d1d-a60b-d576373cf070",
+        "name": "Sleek Steel Shirt",
+        "status": "SHOOTING",
+        "type": "testimonial",
+        "createdOn": "2022-06-15T02:11:04.207Z",
+        "archived": false
+    },
+    {
+        "id": "3407f4b7-7f7c-4609-b559-ecbc069c399d",
+        "name": "Bespoke Bronze Keyboard",
+        "status": "EDITING",
+        "type": "educational",
+        "createdOn": "2022-06-15T02:11:04.208Z",
+        "archived": false
+    },
+    {
+        "id": "1fb331b2-3f98-442d-aa8b-273708c16e28",
+        "name": "Oriental Steel Shoes",
+        "status": "SHOOTING",
+        "type": "training",
+        "createdOn": "2022-06-15T02:11:04.208Z",
+        "archived": false
+    },
+    {
+        "id": "e98991b3-e90a-4b15-9297-d0b0e8352b1d",
+        "name": "Handcrafted Cotton Sausages",
+        "status": "SHOOTING",
+        "type": "recreational",
+        "createdOn": "2022-06-15T02:11:04.209Z",
+        "archived": false
+    },
+    {
+        "id": "386f8a70-1c03-4956-9cd9-435ad878062e",
+        "name": "Rustic Plastic Towels",
+        "status": "COMPLETED",
+        "type": "training",
+        "createdOn": "2022-06-15T02:11:04.209Z",
+        "archived": false
+    },
+    {
+        "id": "d73fcbe4-99e6-401e-b124-320308686353",
+        "name": "Rustic Metal Ball",
+        "status": "SHOOTING",
+        "type": "educational",
+        "createdOn": "2022-06-15T02:11:04.209Z",
+        "archived": false
+    },
+    {
+        "id": "a9cc69cb-eceb-4ab2-ac73-14ed47ec9b44",
+        "name": "Unbranded Granite Salad",
+        "status": "FEEDBACK",
+        "type": "testimonial",
+        "createdOn": "2022-06-15T02:11:04.209Z",
+        "archived": false
+    },
+    {
+        "id": "f19185af-a326-423d-938a-4c0c1b97b461",
+        "name": "Luxurious Metal Cheese",
+        "status": "SHOOTING",
+        "type": "testimonial",
+        "createdOn": "2022-06-15T02:11:04.209Z",
+        "archived": false
+    },
+    {
+        "id": "56c0c37f-5a5e-476b-b9a5-c3e5a8f71798",
+        "name": "Intelligent Frozen Ball",
+        "status": "COMPLETED",
+        "type": "educational",
+        "createdOn": "2022-06-15T02:11:04.209Z",
+        "archived": true
+    },
+    {
+        "id": "820d36ce-2c9e-4013-b06e-fd0a78f166ff",
+        "name": "Incredible Wooden Sausages",
+        "status": "SHOOTING",
+        "type": "testimonial",
+        "createdOn": "2022-06-15T02:11:04.209Z",
+        "archived": false
+    },
+    {
+        "id": "b13239c9-3df7-4315-91af-0d52382868e7",
+        "name": "Gorgeous Steel Gloves",
+        "status": "INCOMPLETE",
+        "type": "recreational",
+        "createdOn": "2022-06-15T02:11:04.209Z",
+        "archived": false
+    },
+    {
+        "id": "91f6ab53-66f5-4edd-bf29-0c60db2e0625",
+        "name": "Unbranded Concrete Chips",
+        "status": "COMPLETED",
+        "type": "educational",
+        "createdOn": "2022-06-15T02:11:04.209Z",
+        "archived": false
+    },
+    {
+        "id": "845a55c5-1616-4ed0-a007-1301b8d6dbdf",
+        "name": "Practical Plastic Salad",
+        "status": "INCOMPLETE",
+        "type": "recreational",
+        "createdOn": "2022-06-15T02:11:04.209Z",
+        "archived": false
+    },
+    {
+        "id": "4a6b967b-1b83-4ec4-ac8f-436d5acab989",
+        "name": "Awesome Soft Bike",
+        "status": "COMPLETED",
+        "type": "educational",
+        "createdOn": "2022-06-15T02:11:04.209Z",
+        "archived": true
+    },
+    {
+        "id": "ee51f5b6-83ad-4086-9dd0-5538c9587643",
+        "name": "Sleek Bronze Cheese",
+        "status": "FEEDBACK",
+        "type": "recreational",
+        "createdOn": "2022-06-15T02:11:04.209Z",
+        "archived": false
+    },
+    {
+        "id": "23fa8008-b42e-42ef-b607-a60bfad95fbc",
+        "name": "Tasty Rubber Shoes",
+        "status": "SHOOTING",
+        "type": "recreational",
+        "createdOn": "2022-06-15T02:11:04.209Z",
+        "archived": false
+    },
+]
+let type = ['educational', 'testimonial', 'training', 'recreational'];
+let status = ['INCOMPLETE', 'SHOOTING', 'EDITING', 'FEEDBACK', 'COMPLETED'];
+
+const search = document.getElementById("search").value
+
+function Search(search, datas) {
+    
+    let filter = {
+        "name": [],
+        "status": [],
+        "type": [],
+        "archived": "",
+    }
+    let input = search.split(" ")
+    let result = []
+    
+    input.forEach(e => {
+        if (e.charAt(2) === ":" && e.charAt(0) === "i") {
+            const inpt = e.replace("is:", "")
+            if (status.includes(inpt.toUpperCase())) {
+                filter["status"].push(inpt.toUpperCase())
+            } else if (type.includes(inpt.toLowerCase())) {
+                filter["type"].push(inpt.toLowerCase())
+            } else if (inpt.toLowerCase() === "archived") {
+                filter["archived"] = true
+            }
+        } else if (e.charAt(3) === ":" && e.charAt(0) === "n") {
+            const inpt = e.replace("not:", "")  
+            if (status.includes(inpt.toUpperCase())) {
+                filter["status"] = status.filter((x) => {return x !== inpt.toUpperCase()})
+            } else if (type.includes(inpt.toLowerCase())) {
+                filter["type"] = type.filter((x) => {return x !== inpt.toLowerCase()})
+            } else if (inpt.toLowerCase() === "archived") {
+                filter["archived"] = false
+            }
+        }else {
+            filter["name"].push(e)
+        }
+    }
+    )
+
+    datas.forEach(d => {
+        if(filter["status"].length !== 0){
+            filter["status"].forEach(s => {
+                if(d.status === s){
+                    result.push(d)
+                }
+            });
+        }else{
+            result.push(d)
+        }
+    });
+    
+    let result2 = []
+    let result3 = []
+    let result4 = []
+    let finalresult
+    res = finalresult
+    
+    result.forEach(d => {
+        if(filter["type"].length !== 0){
+            filter["type"].forEach(t => {
+                if(d.type === t){
+                    result2.push(d)
+                }
+            });
+        }else{
+            result2.push(d)
+        }
+    })
+    
+    result2.forEach(d => {
+        if(filter["archived"].length !== 0){
+            if(d.archived === filter["archived"]){
+                result3.push(d)
+            }
+        }else{
+            result3.push(d)
+        }
+    })
+    if (filter["name"].length !== 0){
+        finalresult = []
+        filter["name"].forEach(n => {
+            result4.push(fuzzysort.go(n, result3, {key:'name'}))
+        });
+        result4.forEach(e => {
+            e.forEach(x => {
+                finalresult.push(x.obj)
+            })
+        });
+    } else {
+        finalresult = result3
+    }
+    return finalresult
+}
